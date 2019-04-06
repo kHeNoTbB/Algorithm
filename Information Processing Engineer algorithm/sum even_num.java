75211a4ba1a8acf8c93654ed363e70e1e6e23b2a
hailey <sj05publu@naver.com>
2019-04-06 18:13:05 +0900

    2019.04.06

---
+++ Information Processing Engineer algorithm/sum even_num.java
@@ -0,0 +1,11 @@
+public class Exam8{
+  public static void main(Sting[] args){
+    int sum=0, a=0;
+    while(a<10) {
+      a++;
+      if(a%2==1) continue;
+      sum+=a;
+    }
+    System.out.printIn(sum);
+  }
+}
