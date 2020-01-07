18cfea5ef9d5cfa1370e8ad7904fbc1e846bfb8c
hailey <sj05publu@naver.com>
2020-01-07 14:21:00 +0900

    Create 자릿수더하기.java

---
+++ programmers/level1/자릿수더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(true) {
+            answer += n%10;
+            n/=10;
+            if(n/10==0) {
+                answer += n%10;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
