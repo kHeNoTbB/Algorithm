3d6e7bb1273b0a4e558f134eebe46e3c1b522c5e
hailey <sj05publu@naver.com>
2020-01-07 13:16:44 +0900

    Create 수박수박수박수박수박수?.java

---
+++ programmers/level1/수박수박수박수박수박수?.java
@@ -0,0 +1,14 @@
+class Solution {
+  public String solution(int n) {
+      String answer = "";
+      
+      if(n!=0) {
+        for(int i=0; i<n/2; i++) {
+            answer+="수박";
+        }
+        if(n%2==1) answer+="수";
+      }
+      
+      return answer;
+  } 
+}
