2ab0c99bd9897ea0204d02fd38fe44a50a77d67f
hailey <sj05publu@naver.com>
2020-02-29 13:55:51 +0900

    Update 스킬트리.java

--- programmers/level2/스킬트리.java
+++ programmers/level2/스킬트리.java
@@ -1,24 +1,32 @@
-class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        loop:for(int i=0; i<skill_trees.length; i++) {
-            String test = skill_trees[i];
-            boolean[] ck = new boolean[skill.length()];
-            
-            for(int j=0; j<test.length(); j++) {    
-                char c = test.charAt(j);
-                for(int k=0; k<skill.length(); k++) {
-                    if(c!=skill.charAt(k)) continue;
-                    for(int l=k-1; l>=0; l--) {
-                        if(!ck[l]) {
-                            continue loop;  
-                        }
-                    }
-                    ck[k]=true;
-                }
-            }
-            answer ++;
-        }
-        return answer;
-    }
+package programmers;
+
+/*
+ * 문자열의 길이는 .length()
+ * 그 외 배열등의 길이는 .length
+ */
+public class 스킬트리 {
+	class Solution {
+	    public int solution(String skill, String[] skill_trees) {
+	        int answer = 0;
+	        loop:for(int i=0; i<skill_trees.length; i++) {
+	            String test = skill_trees[i];
+	            boolean[] ck = new boolean[skill.length()];
+	            
+	            for(int j=0; j<test.length(); j++) {    
+	                char c = test.charAt(j);
+	                for(int k=0; k<skill.length(); k++) {
+	                    if(c!=skill.charAt(k)) continue;
+	                    for(int l=k-1; l>=0; l--) {
+	                        if(!ck[l]) {
+	                            continue loop;  
+	                        }
+	                    }
+	                    ck[k]=true;
+	                }
+	            }
+	            answer ++;
+	        }
+	        return answer;
+	    }
+	}
 }
