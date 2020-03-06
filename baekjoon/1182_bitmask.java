2d333fe4314f038b7a2685d3e07a85dfb59d05f2
hailey <sj05publu@naver.com>
2020-03-07 05:15:41 +0900

    Update 1182_bitmask.java

--- baekjoon/1182_bitmask.java
+++ baekjoon/1182_bitmask.java
@@ -4,10 +4,10 @@ public class Main {
 		int answer = 0;
 		Scanner sc = new Scanner(System.in);
 		String num = sc.nextLine().trim();
-		int S = Integer.parseInt(num.split(" ")[1]);
+		int S = Integer.parseInt(num.split(" ")[1]); //string보다 그냥 원래 하던대로 int[] 로 연산하는 것이 빠르다.
 		String[] arr = sc.nextLine().split(" ");
 		
-		for(int i=0; i<(1<<arr.length); i++) {
+		for(int i=0; i<(1<<arr.length); i++) { // i=1부터 시작하면 공백 안들어가므로 flag를 따로 쓰지 않아도 된다.
 			int sum = 0 ;
 			boolean flag = false;
 			for(int j=0; j<arr.length; j++) {
