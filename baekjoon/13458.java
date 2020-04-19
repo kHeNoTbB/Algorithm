ecd533e92e6f59d2d1d000b741ba96a32c680a0c
hailey <sj05publu@naver.com>
2020-04-19 19:25:58 +0900

    시험감독

--- baekjoon/13458.java
+++ baekjoon/13458.java
@@ -2,26 +2,18 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N =Integer.parseInt(sc.nextLine()); //시험장의 개수
+		int N =Integer.parseInt(sc.nextLine());
 		int[] A = new int[N];
 		StringTokenizer st = new StringTokenizer(sc.nextLine());
 		for(int i=0; i<N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-
-		st = new StringTokenizer(sc.nextLine());
-		int B = Integer.parseInt(st.nextToken()); //총 감독관이 감시할 수 있는 수
-		int C = Integer.parseInt(st.nextToken());//부 감독관이 감시할 수 있는 수
-		
-		long result = 0;
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		long result = N;
 		for(int a=0; a<A.length; a++) {
-			int student = A[a];
-			student -= B;
-			result++;
-			if(student>0) {
-				result += student/C;
-				if(student%C!=0) result++;
-			}
+			A[a] -= B;
+			if(A[a]>0) result += A[a]%C==0?A[a]/C:A[a]/C+1;
 		}
 		System.out.print(result);
 	}
